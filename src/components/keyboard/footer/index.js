da14b3c9310f3b04d8953be7bc724d556adedda0
tiennhatdn98 <nhat.truongdn98@gmail.com>
2021-12-03 16:32:45 +0700

    add email content

--- src/components/keyboard/footer/index.js
+++ src/components/keyboard/footer/index.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './index.module.css';
 
+const emailRegex =
+  /^[a-z][a-z0-9_\.]{5,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}$/gm;
+
 function Footer() {
+  const [email, setEmail] = useState('');
+  const [content, setContent] = useState('');
+  const [emailError, setEmailError] = useState(false);
+  const [contentError, setContentError] = useState(false);
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (emailError) console.log('Loi email');
+    else console.log('Email ok');
+    if (contentError) console.log('Loi content');
+    else console.log('Content ok');
+  };
+
+  useEffect(() => {
+    emailRegex.test(email.toLowerCase())
+      ? setEmailError(false)
+      : setEmailError(true);
+
+    content ? setContentError(false) : setContentError(true);
+  }, [email, content]);
+
   return (
     <div className={styles['container']}>
       <div className={styles['content']}>
@@ -30,16 +61,24 @@ function Footer() {
             type="text"
             className={styles['input']}
             placeholder="Your email"
+            value={email}
+            onChange={handleEmail}
+            style={emailError ? { border: 'red' } : { border: 'none' }}
           />
         </div>
         <div className={styles['input-wrap']}>
           <textarea
             className={styles['textarea']}
             placeholder="Content"
+            value={content}
+            onChange={handleContent}
+            style={contentError ? { border: 'red' } : { border: 'none' }}
           ></textarea>
         </div>
         <div className={styles['input-wrap']}>
-          <button className={styles['send-button']}>Send</button>
+          <button className={styles['send-button']} onClick={handleSubmit}>
+            Send
+          </button>
         </div>
       </div>
       <img
