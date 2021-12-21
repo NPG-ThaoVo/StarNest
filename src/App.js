b4b1156afb9927c245cb1eff03f947405e2e31e8
quangviet93 <quangviet.nguyen19@gmail.com>
2021-12-21 11:47:38 +0700

    add-component-privatepolicy,termsofuse-and-handle-top-product

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages';
-import Kebo from './pages/kebo';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import Kebo from "./pages/kebo";
+import Content from "./components/home/privatepolicy";
+import Termsofuse from "./components/home/termsofuse";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/kebo" element={<Kebo />} />
+      <Route path="kebo/privatepolicy" element={<Content />} />
+      <Route path="kebo/termsofuse" element={<Termsofuse />} />
     </Routes>
   );
 }
--- src/components/keyboard/popup/index.js
+++ src/components/keyboard/popup/index.js
@@ -1,38 +1,38 @@
-import React from 'react';
-import styles from './index.module.css';
+import React from "react";
+import styles from "./index.module.css";
 
 function Popup(props) {
   return (
-    <div className={styles['popup-wrapper']}>
-      <div className={styles['popup']}>
-        <div className={styles['popup-container']}>
+    <div className={styles["popup-wrapper"]}>
+      <div className={styles["popup"]}>
+        <div className={styles["popup-container"]}>
           <img
             src="./imgs/close-icon.svg"
             alt=""
-            className={styles['popup-close']}
+            className={styles["popup-close"]}
             onClick={props.closePopup}
           />
-          <div className={styles['popup-main-wrapper']}>
-            <div className={styles['popup-image-wrapper']}>
+          <div className={styles["popup-main-wrapper"]}>
+            <div className={styles["popup-image-wrapper"]}>
               <img
                 src="./imgs/icon-popup.svg"
                 alt=""
-                style={{ display: 'block' }}
-                className={styles['popup-icon']}
+                style={{ display: "block" }}
+                className={styles["popup-icon"]}
               />
             </div>
-            <div className={styles['popup-alert']}>
-              <div className={styles['popup-title']}>
-                <strong>Congratulation!!!</strong>
+            <div className={styles["popup-alert"]}>
+              <div className={styles["popup-title"]}>
+                <strong>Sent!!!</strong>
               </div>
-              <span className={styles['popup-description']}>
+              <span className={styles["popup-description"]}>
                 We have received the information and are processing it in two
                 working days
               </span>
             </div>
-            <div className={styles['popup-button-wrapper']}>
+            <div className={styles["popup-button-wrapper"]}>
               <button
-                className={styles['popup-button']}
+                className={styles["popup-button"]}
                 onClick={props.closePopup}
               >
                 <strong>GOT IT THANKS!</strong>
