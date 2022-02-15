d68d06325ceb7d4f07506a0489b350ff611274bb
htphong01 <thanhphonga4.hbt@gmail.com>
2022-02-15 22:38:52 +0700

    fix

--- src/components/calendar/features/index.js
+++ src/components/calendar/features/index.js
@@ -1,9 +1,22 @@
 import styles from "./index.module.css";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Features() {
 
   const [isRightFeature, setIsRightFeature] = useState(false);
+  const [isOnMobile, setIsOnMobile] = useState(false);
+
+  useEffect(() => {
+    if(window.innerWidth <= 700) setIsOnMobile(true);
+    window.addEventListener('resize', () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth <= 700) setIsOnMobile(true);
+      else setIsOnMobile(false);
+    });
+    return () => {
+      window.removeEventListener('resize', () => { });
+    }
+  }, []);
 
   const data = [
     {
@@ -30,10 +43,6 @@ export default function Features() {
       color: "#4DD1CD",
       content: "Background <br>effects",
     },
-  ];
-
-  const data1 = [
-    data[2],
     {
       color: '#4ABA78',
       content: 'Longlife <br> journal'
@@ -42,7 +51,6 @@ export default function Features() {
       color: '#F86F90',
       content: 'Memo'
     },
-    data[5],
     {
       color: '#FFC15D',
       content: 'Create a new <br>habit'
@@ -51,14 +59,14 @@ export default function Features() {
       color: '#EB7E4F',
       content: 'Sync <br> iCloud'
     }
-  ]
+  ];
 
   return (
     <>
       <div className={styles["container"]}>
         <div className={styles["title"]}>Features you’ll love</div>
-        <div className={styles["features"]}>
-          {!isRightFeature ?
+        <div className={styles[isRightFeature ? "right-features" : "features"]}>
+          {isOnMobile ?
             data.map((feature, i) => (
               <Feature
                 order={i + 1}
@@ -67,29 +75,58 @@ export default function Features() {
                 key={i}
               />
             )) :
-            data1.map((feature, i) => (
-              <Feature
-                order={i + 1}
-                content={feature.content}
-                color={feature.color}
-                key={i}
-              />
-            ))}
-          <div className={styles["right-icon"]} onClick={() => setIsRightFeature(!isRightFeature)} >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="22"
-              height="20"
-              viewBox="0 0 22 20"
-              fill="none"
-            >
-              <path
-                d="M11 1L20 10L11 19M0 10H20H0Z"
-                stroke="#333333"
-                strokeWidth="2"
-              />
-            </svg>
-          </div>
+            <>
+              {!isRightFeature ?
+                data?.slice(0, 6).map((feature, i) => (
+                  <Feature
+                    order={i + 1}
+                    content={feature.content}
+                    color={feature.color}
+                    key={i}
+                  />
+                )) :
+                data?.slice(6).map((feature, i) => (
+                  <Feature
+                    order={i + 7}
+                    content={feature.content}
+                    color={feature.color}
+                    key={i}
+                  />
+                ))}
+              {!isRightFeature ?
+                <div className={styles["right-icon"]} onClick={() => setIsRightFeature(true)} >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="22"
+                    height="20"
+                    viewBox="0 0 22 20"
+                    fill="none"
+                  >
+                    <path
+                      d="M11 1L20 10L11 19M0 10H20H0Z"
+                      stroke="#333333"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                </div> :
+                <div className={styles["left-icon"]} onClick={() => setIsRightFeature(false)} >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="22"
+                    height="20"
+                    viewBox="0 0 22 20"
+                    fill="none"
+                  >
+                    <path
+                      d="M11 1L20 10L11 19M0 10H20H0Z"
+                      stroke="#333333"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                </div>
+              }
+            </>
+          }
         </div>
       </div>
     </>
@@ -101,7 +138,7 @@ function Feature({ order, content, color }) {
     <>
       <div className={styles["wrap"]}>
         <div className={styles["order"]}>
-          <div style={{ background: color }}>{"0" + order}</div>
+          <div style={{ background: color }}>{(order === 10 ? "" : "0") + order}</div>
           <div style={{ background: color }}></div>
         </div>
         <div className={styles["content"]} dangerouslySetInnerHTML={{ __html: content }}></div>
