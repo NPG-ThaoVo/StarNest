fb7f3837400f561ae90b8d90aca0abd89ebfa00d
tiennhatdn98 <nhat.truongdn98@gmail.com>
2022-04-15 14:29:03 +0700

    add link to free download button

--- src/components/calendar/header/index.js
+++ src/components/calendar/header/index.js
@@ -20,23 +20,23 @@ export default function Header({ setIsOnView }) {
   }
   function getMobileOperatingSystem() {
     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
     // Windows Phone must come first because its UA also contains "Android"
     if (/windows phone/i.test(userAgent)) {
         return "Windows Phone";
     }
 
-    if (/android/i.test(userAgent)) {
+    if (/android/i.test(userAgent) || /windows/i.test(userAgent)) {
         return "Android";
     }
 
     // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    if (/iPad|iPhone|iPod/.test(userAgent) || /macintosh/i.test(userAgent) && !window.MSStream) {
         return "iOS";
     }
 
     return "unknown";
-}
+  }
+
   useEffect(() => {
     const isOnView = () => {
       const isOn = isElementInViewport();
@@ -96,7 +96,8 @@ export default function Header({ setIsOnView }) {
             <a href="/calendar/privatepolicy">Private Policy</a>
             <div className={styles["download-btn"]}>
               <a
-                href="https://apps.apple.com/us/app/id1585939051"
+                // href="https://apps.apple.com/us/app/id1585939051"
+                href={os}
                 target="_blank"
                 rel="noreferrer"
               >
@@ -118,7 +119,8 @@ export default function Header({ setIsOnView }) {
         </div>
         <div className={styles["btn-blur"]} onClick={onClick} ref={btn}>
           <a
-            href="https://apps.apple.com/us/app/id1585939051"
+            // href="https://apps.apple.com/us/app/id1585939051"
+            href={os}
             target="_blank"
             rel="noreferrer"
           >
